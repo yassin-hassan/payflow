a33269c3fd0eedf7a1f4552b1c075d3fed510900
Yassin Hassan <ynhassan22@gmail.com>
2026-08-10 15:33:47 +0200

    chore: scaffold spring boot project

---
+++ src/main/java/com/payflow/PayflowApplication.java
@@ -0,0 +1,13 @@
+package com.payflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payflow/PayflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
